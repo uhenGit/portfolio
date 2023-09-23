1cd6385101ff50f5452c773a4fe9ebcfebeb89e9
uhenGit <uhen.dn@gmail.com>
2023-09-23 16:15:44 +0300

    - fix typos - add work experience - clean code - refactor

--- js/script.js
+++ js/script.js
@@ -1,46 +1,6 @@
-// fetch multilanguage data from my-json-server
 
-const fetchAllData = () => {
-  if (localStorage.getItem("lang") === null) {
-    select = "en";
-  } else {
-    select = localStorage.getItem("lang");
-  }
-  //console.log(localStorage.getItem("portfolio-db"));
-  if (localStorage.getItem("portfolio-db") === null) {
-    fetch("https://my-json-server.typicode.com/uhenGit/portfolio-db/db")
-      .then((res) => res.json())
-      .then((data) => {
-        localStorage.setItem("portfolio-db", JSON.stringify(data));
-        setContent(data);
-      })
-      .catch((err) => console.log(err));
-  } else {
-    setContent(JSON.parse(localStorage.getItem("portfolio-db")));
-  }
-};
-//window.onload = fetchAllData;
-let select = "";
-// document.querySelector(".btn_toggle").addEventListener("click", (e) => {
-//   select = e.target.children[0].innerText;
-//   localStorage.setItem("lang", select);
-//   fetchAllData();
-// });
-function setContent(info) {
-  const textField = document.querySelectorAll(".text");
-  for (let i = 0; i < textField.length; i++) {
-    textField[i].innerText = Object.values(info[select])[i];
-  }
-  if (select === "ru") {
-    document.querySelector(".btn_toggle").children[0].innerText = "en";
-  } else {
-    document.querySelector(".btn_toggle").children[0].innerText = "ru";
-  }
-  console.log(Object.values(info[select]));
-  console.log(textField);
-}
 const menuBtn = document.querySelector(".line_wrap");
-const nav = document.querySelector("nav");
+const nav = document.querySelector(".nav");
 const pic = document.querySelector(".my_pic");
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menu_item");
